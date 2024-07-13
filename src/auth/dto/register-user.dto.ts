093673285f37236b9cdcbe1c730fdd5424554d9f
martinfyic <martin.f.yic@gmail.com>
2024-07-12 22:46:17 -0300

    feat: :label: create register-user.dto.ts

--- src/auth/dto/register-user.dto.ts
+++ src/auth/dto/register-user.dto.ts
@@ -1,1 +1,80 @@
-export class RegisterUserDto {}
+import { Role } from '@prisma/client';
+import {
+  IsBoolean,
+  IsEmail,
+  IsEnum,
+  IsISO8601,
+  IsOptional,
+  IsString,
+  IsUrl,
+  Matches,
+  MaxLength,
+  MinLength,
+  ValidateNested,
+} from 'class-validator';
+import { Type } from 'class-transformer';
+import { AddressUserDto } from './address-user.dto';
+
+export class RegisterUserDto {
+  @IsString()
+  @MinLength(2)
+  @MaxLength(50)
+  name: string;
+
+  @IsString()
+  @MinLength(2)
+  @MaxLength(50)
+  lastName: string;
+
+  @IsString()
+  @MinLength(2)
+  @MaxLength(50)
+  username: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @MinLength(6)
+  @MaxLength(50)
+  @Matches(/(?:(?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message:
+      'The password must have a Uppercase, lowercase letter and a number',
+  })
+  password: string;
+
+  @IsString()
+  @MinLength(6)
+  @MaxLength(50)
+  @Matches(/(?:(?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message:
+      'The verify password must have a Uppercase, lowercase letter and a number',
+  })
+  password2: string;
+
+  @IsOptional()
+  @IsString()
+  @MinLength(8)
+  phone?: string;
+
+  @IsOptional()
+  @IsISO8601()
+  dateOfBirth: Date;
+
+  @IsOptional()
+  @IsUrl()
+  profilePictureURL?: string;
+
+  @IsOptional()
+  @ValidateNested()
+  @Type(() => AddressUserDto)
+  address?: AddressUserDto;
+
+  @IsOptional()
+  @IsBoolean()
+  newsletterSubscription?: boolean;
+
+  @IsOptional()
+  @IsEnum(Role)
+  role?: Role;
+}
