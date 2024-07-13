dbf3c73425add317654310d3ea430c1e0f45aaf0
martinfyic <martin.f.yic@gmail.com>
2024-07-12 23:00:39 -0300

    refactor: :wheelchair: change method names

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -14,33 +14,43 @@ import { LoginUserDto, RegisterUserDto, UpdateUserDto } from './dto';
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  /**
+   *
+   * ==> Registro de usuario
+   *
+   */
   @Post('register')
   registerUser(@Body() registerUserDto: RegisterUserDto) {
     return this.authService.registerUser(registerUserDto);
   }
 
+  /**
+   *
+   * ==> Login de usuario
+   *
+   */
   @Post('login')
   loginUser(@Body() loginUserDto: LoginUserDto) {
     return this.authService.loginUser(loginUserDto);
   }
 
   @Get()
-  findAll() {
-    return this.authService.findAll();
+  findAllUsers() {
+    return this.authService.findAllUsers();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.authService.findOne(+id);
+  findOneUser(@Param('id') id: string) {
+    return this.authService.findOneUser(+id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+  updateUser(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.authService.update(+id, updateUserDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
+  removeUser(@Param('id') id: string) {
     return this.authService.remove(+id);
   }
 }
