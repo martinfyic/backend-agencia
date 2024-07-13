90ab6dc8942cbe797e368e7e1edfb9f999a42a55
martinfyic <martin.f.yic@gmail.com>
2024-07-12 22:56:16 -0300

    feat: :construction: metod to registerUser at auth.service.ts
    work in progress, metod can register user at db, need save jwt token at response

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,15 @@
-import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import {
+  BadRequestException,
+  HttpStatus,
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+  OnModuleInit,
+} from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+
+import * as bcrypt from 'bcrypt';
+
 import { RegisterUserDto } from './dto/register-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { LoginUserDto } from './dto';
@@ -17,19 +27,74 @@ export class AuthService extends PrismaClient implements OnModuleInit {
     }
   }
 
-  registerUser(registerUserDto: RegisterUserDto) {
-    return 'This action adds a new user';
+  async registerUser(registerUserDto: RegisterUserDto) {
+    if (registerUserDto.password !== registerUserDto.password2) {
+      throw new BadRequestException({
+        status: HttpStatus.BAD_REQUEST,
+        message: 'The password does not match',
+      });
+    }
+
+    try {
+      const user = await this.user.findUnique({
+        where: {
+          email: registerUserDto.email,
+        },
+      });
+
+      if (user) {
+        throw new BadRequestException({
+          status: HttpStatus.BAD_REQUEST,
+          message: 'User already exists',
+        });
+      }
+
+      const registerUser = await this.user.create({
+        data: {
+          email: registerUserDto.email,
+          lastName: registerUserDto.lastName.toLowerCase(),
+          name: registerUserDto.name.toLowerCase(),
+          password: bcrypt.hashSync(registerUserDto.password, 10),
+          username: registerUserDto.username.toLowerCase() || undefined,
+          address: registerUserDto.address
+            ? { create: registerUserDto.address }
+            : undefined,
+          dateOfBirth: registerUserDto.dateOfBirth || undefined,
+          lastLogin: new Date(),
+          phone: registerUserDto.phone || undefined,
+          profilePictureURL: registerUserDto.profilePictureURL || undefined,
+        },
+        include: {
+          address: true,
+        },
+      });
+
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { password, ...restUserData } = registerUser;
+
+      // TODO: devolver token JWT cuando se registra el usuario.
+      return {
+        metadata: {
+          status: HttpStatus.OK,
+          date: new Date().toISOString(),
+        },
+        user: restUserData,
+        token: '',
+      };
+    } catch (error) {
+      this.handleDBExceptions(error);
+    }
   }
 
   loginUser(loginUserDto: LoginUserDto) {
     return 'This action login user';
   }
 
-  findAll() {
+  findAllUsers() {
     return `This action returns all auth`;
   }
 
-  findOne(id: number) {
+  findOneUser(id: number) {
     return `This action returns a #${id} auth`;
   }
 
@@ -39,5 +104,15 @@ export class AuthService extends PrismaClient implements OnModuleInit {
 
   remove(id: number) {
     return `This action removes a #${id} auth`;
+  }
+
+  private handleDBExceptions(error: any): never {
+    if (error.code === '23505') throw new BadRequestException(error.detail);
+    if (error?.response?.status === HttpStatus.BAD_REQUEST)
+      throw new BadRequestException(error.response);
+    this.logger.error(error);
+    throw new InternalServerErrorException(
+      'Unexpected error, check server logs',
+    );
   }
 }
